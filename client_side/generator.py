6cc91ae70cc56a2c878ad7f9ab759c7cfc0ebca0
zhujinghui <zhujinghui1115@gmail.com>
2018-12-14 12:48:20 -0500

    submit

---
+++ client_side/generator.py
@@ -0,0 +1,26 @@
+import time
+import grequests
+
+# while True:
+#     r = requests.get("http://127.0.0.1:5000/")
+#     print(r.text)
+#     time.sleep(1)
+#
+#     r1 = requests.get("http://127.0.0.1:5001/")
+#     print(r1.text)
+#     time.sleep(1)
+
+while True:
+    start_time = time.time()
+
+    urls = [
+               "http://127.0.0.1:5000/",
+               "http://127.0.0.1:5001/"
+           ] * 20
+
+    rs = (grequests.head(u) for u in urls)
+    print(grequests.map(rs))
+
+    print(time.time() - start_time)
+    print("wait 5s")
+    time.sleep(5)
