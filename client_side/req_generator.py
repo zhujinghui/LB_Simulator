ce1e9d66637c707a5d008f04951d4c1f4dc7947e
zhujinghui <zhujinghui1115@gmail.com>
2019-01-13 23:24:16 +0900

    submit

--- client_side/req_generator.py
+++ client_side/req_generator.py
@@ -8,7 +8,7 @@ import os
 import time
 
 start_time = time.time()
-
 while True:
-    os.system("ab -t 1 -c 1000 http://load-balancer-1697078231.us-east-1.elb.amazonaws.com/")
-
+    os.system("ab -t 1 -c 2000 http://load-balancer-484049472.us-east-1.elb.amazonaws.com/")
+    time.sleep(0.5)
+    print( time.time()-start_time)
